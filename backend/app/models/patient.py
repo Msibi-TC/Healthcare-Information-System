e0fbd35995ff99957d036967a8e014c27bbecf7a
Thando Msibi <thandocomfortmsibi@gmail.com>
2026-03-10 09:01:58 +0200

    Finished the patient model.

--- backend/app/models/patient.py
+++ backend/app/models/patient.py
@@ -9,4 +9,19 @@ class Patient(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), unique=True, nullable=False)
+    blood_group = Column(String)
+    height  = Column(Float)
+    weight = Column(Float)
+    emergency_contact_name = Column(String)
+    emergency_contact_phone = Column(String)
+    allergies = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    #Relationships
+    user = relationship("User", back_populates="patient")
+    appointments = relationship("Appointment", back_populates="patient")
+    consultations = relationship("Consulation", back_populates="patient")
+    surgeries = relationship("Surgery", back_populates="patient")
+    medical_records = relationship("MedicalRecord", back_populates="patient")
     
